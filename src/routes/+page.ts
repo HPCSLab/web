632254745535eb42fabd8e755ad08a8bb72fff50
KoyamaSohei <koyamaso0309@gmail.com>
2023-12-13 00:35:16 +0900

    Update news slug

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,14 +2,14 @@ import type { PageLoad } from './$types';
 import { slugFromPath } from '$lib/slugFromPath';
 
 export const load: PageLoad = async () => {
-	const news_data = import.meta.glob(`/src/news/*.{md,svx,svelte.md}`);
+	const news_data = import.meta.glob(`/src/news/*/*.{md,svx,svelte.md}`);
     let news: { date: string, title: string, path: string }[] = [];
 	for (const [path, resolver] of Object.entries(news_data)) {
 		const content: any = await resolver();
         news.push({
             date: content.metadata.date,
             title: content.metadata.title,
-            path: `/news/${slugFromPath(path)}`,
+            path: `/news/${content.metadata.date.slice(0, 4)}/${slugFromPath(path)}`,
         });
 	}
 
