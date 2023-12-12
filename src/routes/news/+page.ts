22c9e42c0755c2c7cfebd1ce08fded40678b6abe
KoyamaSohei <koyamaso0309@gmail.com>
2023-12-13 00:54:14 +0900

    Add news page

---
+++ src/routes/news/+page.ts
@@ -0,0 +1,27 @@
+import type { PageLoad } from './$types';
+import { slugFromPath } from '$lib/slugFromPath';
+
+export const load: PageLoad = async () => {
+	const news_data = import.meta.glob(`/src/news/*/*.{md,svx,svelte.md}`);
+    let news: { date: string, title: string, path: string }[] = [];
+	for (const [path, resolver] of Object.entries(news_data)) {
+		const content: any = await resolver();
+        news.push({
+            date: content.metadata.date,
+            title: content.metadata.title,
+            path: `/news/${content.metadata.date.slice(0, 4)}/${slugFromPath(path)}`,
+        });
+	}
+
+    news.sort((a, b) => {
+        const ad = Date.parse(a.date);
+        const bd = Date.parse(b.date);
+        if (ad == bd) return 0;
+        if (ad < bd) return 1;
+        return -1;
+    });
+
+    return {
+        news,
+    };
+};
