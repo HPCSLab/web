9f89719517c0f2903cfc6b63274445f630d56444
KoyamaSohei <koyamaso0309@gmail.com>
2023-12-12 17:25:07 +0900

    Init header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,125 @@
+<header class="site-header scale responsive-menu" role="banner">
+	<div class="container">
+		<div class="site-branding-container">
+			<div class="site-branding">
+				<a href="/" rel="home">
+					<h1 class="site-title">HPCS Lab.</h1>
+				</a>
+			</div>
+		</div>
+		<nav id="site-navigation" class="main-navigation" role="navigation">
+			<button class="menu-toggle"></button>
+			<div class="menu-menu-container">
+				<ul id="menu-menu" class="menu nav-menu" aria-expanded="false">
+					<li
+						id="menu-item-17"
+						class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-15 current_page_item menu-item-17"
+					>
+						<a href="/">Home</a>
+					</li>
+					<li
+						id="menu-item-36"
+						class="menu-item menu-item-type-post_type menu-item-object-page menu-item-36"
+					>
+						<a href="/research/">Research</a>
+					</li>
+					<li
+						id="menu-item-38"
+						class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-38"
+					>
+						<a href="/members/">Members</a>
+						<ul class="sub-menu">
+							<li
+								id="menu-item-37"
+								class="menu-item menu-item-type-post_type menu-item-object-page menu-item-37"
+							>
+								<a href="/members/alumni/">Alumni</a>
+							</li>
+						</ul>
+					</li>
+					<li
+						id="menu-item-113"
+						class="menu-item menu-item-type-post_type menu-item-object-page menu-item-113"
+					>
+						<a href="/publications/">Publications</a>
+					</li>
+					<li
+						id="menu-item-730"
+						class="menu-item menu-item-type-custom menu-item-object-custom menu-item-730"
+					>
+						<a target="_blank" href="/bachelor/#!index.md">Open Lab.</a>
+					</li>
+					<li
+						id="menu-item-39"
+						class="menu-item menu-item-type-post_type menu-item-object-page menu-item-39"
+					>
+						<a href="/access/">Access</a>
+					</li>
+					<li
+						id="menu-item-40"
+						class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-40"
+					>
+						<a href="/#">Link</a>
+						<ul class="sub-menu">
+							<li
+								id="menu-item-41"
+								class="menu-item menu-item-type-custom menu-item-object-custom menu-item-41"
+							>
+								<a target="_blank" href="http://www.tsukuba.ac.jp/">筑波大学</a>
+							</li>
+							<li
+								id="menu-item-42"
+								class="menu-item menu-item-type-custom menu-item-object-custom menu-item-42"
+							>
+								<a target="_blank" href="http://www.sie.tsukuba.ac.jp/">システム情報工学研究科</a>
+							</li>
+							<li
+								id="menu-item-43"
+								class="menu-item menu-item-type-custom menu-item-object-custom menu-item-43"
+							>
+								<a target="_blank" href="http://www.cs.tsukuba.ac.jp/">コンピュータサイエンス専攻</a
+								>
+							</li>
+							<li
+								id="menu-item-44"
+								class="menu-item menu-item-type-custom menu-item-object-custom menu-item-44"
+							>
+								<a target="_blank" href="https://www.coins.tsukuba.ac.jp/">情報科学類</a>
+							</li>
+							<li
+								id="menu-item-45"
+								class="menu-item menu-item-type-custom menu-item-object-custom menu-item-45"
+							>
+								<a target="_blank" href="http://www.ccs.tsukuba.ac.jp/">計算科学研究センター</a>
+							</li>
+							<li
+								id="menu-item-732"
+								class="menu-item menu-item-type-custom menu-item-object-custom menu-item-732"
+							>
+								<a target="_blank" href="/bachelor/#!index.md">卒研配属情報</a>
+							</li>
+							<li
+								id="menu-item-748"
+								class="menu-item menu-item-type-custom menu-item-object-custom menu-item-748"
+							>
+								<a href="/experiment/text.html">情報システム実験B</a>
+							</li>
+							<li
+								id="menu-item-46"
+								class="menu-item menu-item-type-custom menu-item-object-custom menu-item-46"
+							>
+								<a target="_blank" href="/internal/pukiwiki/">Internal Page</a>
+							</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</nav>
+	</div>
+</header>
+
+<style>
+    .menu-item-has-children:hover ul {
+        display: block;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script lang="ts">
+    import Header from '$lib/components/Header.svelte'
+	import PageHead from '$lib/components/PageHead.svelte';
+</script>
+
+<PageHead title={"HPCS Lab."} description={"HPCS Lab. Webページ"} />
+<Header />
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
