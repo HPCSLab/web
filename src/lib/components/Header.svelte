934706d1afa88727d2fde9c634120c251c08fd06
KoyamaSohei <koyamaso0309@gmail.com>
2023-12-13 11:43:18 +0900

    Fix responsive nav

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,3 +1,10 @@
+<script>
+	let toggled = false;
+	function clickButton() {
+		toggled = !toggled;
+	}
+</script>
+
 <header class="site-header scale responsive-menu" role="banner">
 	<div class="container">
 		<div class="site-branding-container">
@@ -7,10 +14,10 @@
 				</a>
 			</div>
 		</div>
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle"></button>
+		<nav id="site-navigation" class="main-navigation {toggled ? 'toggled' : ''}" role="navigation">
+			<button class="menu-toggle" aria-expanded={toggled ? "true" : "false"} on:click={clickButton}></button>
 			<div class="menu-menu-container">
-				<ul id="menu-menu" class="menu nav-menu" aria-expanded="false">
+				<ul id="menu-menu" class="menu nav-menu" aria-expanded={toggled ? "true" : "false"}>
 					<li
 						id="menu-item-17"
 						class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-15 current_page_item menu-item-17"
@@ -47,7 +54,9 @@
 						id="menu-item-730"
 						class="menu-item menu-item-type-custom menu-item-object-custom menu-item-730"
 					>
-						<a target="_blank" href="https://www.hpcs.cs.tsukuba.ac.jp/bachelor/#!index.md">Open Lab.</a>
+						<a target="_blank" href="https://www.hpcs.cs.tsukuba.ac.jp/bachelor/#!index.md"
+							>Open Lab.</a
+						>
 					</li>
 					<li
 						id="menu-item-39"
@@ -96,19 +105,25 @@
 								id="menu-item-732"
 								class="menu-item menu-item-type-custom menu-item-object-custom menu-item-732"
 							>
-								<a target="_blank" href="https://www.hpcs.cs.tsukuba.ac.jp/bachelor/#!index.md">卒研配属情報</a>
+								<a target="_blank" href="https://www.hpcs.cs.tsukuba.ac.jp/bachelor/#!index.md"
+									>卒研配属情報</a
+								>
 							</li>
 							<li
 								id="menu-item-748"
 								class="menu-item menu-item-type-custom menu-item-object-custom menu-item-748"
 							>
-								<a href="https://www.hpcs.cs.tsukuba.ac.jp/experiment/text.html">情報システム実験B</a>
+								<a href="https://www.hpcs.cs.tsukuba.ac.jp/experiment/text.html"
+									>情報システム実験B</a
+								>
 							</li>
 							<li
 								id="menu-item-46"
 								class="menu-item menu-item-type-custom menu-item-object-custom menu-item-46"
 							>
-								<a target="_blank" href="https://www.hpcs.cs.tsukuba.ac.jp/internal/pukiwiki/">Internal Page</a>
+								<a target="_blank" href="https://www.hpcs.cs.tsukuba.ac.jp/internal/pukiwiki/"
+									>Internal Page</a
+								>
 							</li>
 						</ul>
 					</li>
@@ -119,7 +134,7 @@
 </header>
 
 <style>
-    .menu-item-has-children:hover ul {
-        display: block;
-    }
+	.menu-item-has-children:hover ul {
+		display: block;
+	}
 </style>
