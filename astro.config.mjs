014fedd017e9bb63a5dbf968f9ae43e97475f4e8
Masaki Nakano <admin@namachan10777.dev>
2024-01-12 11:22:48 +0900

    simple design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import YamlPlugin from "@rollup/plugin-yaml";
-
 import mdx from "@astrojs/mdx";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [YamlPlugin()],
   },
-  integrations: [mdx()],
+  integrations: [mdx(), icon()],
 });
